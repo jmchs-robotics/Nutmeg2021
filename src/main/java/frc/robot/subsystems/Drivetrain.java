e5f1d6fe320afcd51ac9551f8cfb1119835de1ae
Ian Galaviz <ig25049@judgememorial.com>
2022-10-25 08:39:18 -0700

    + Change the speed limit (hopefully)

--- src/main/java/frc/robot/subsystems/Drivetrain.java
+++ src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -5,7 +5,7 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public abstract class Drivetrain extends SubsystemBase {
 	private final double width;
 	private final double length;
-	private double speedMultiplier = 1;
+	private double speedMultiplier = 0.75;
 
 	public Drivetrain(double width, double length) {
 		this.width = width;
